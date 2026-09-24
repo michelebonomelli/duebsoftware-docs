3a0a092a4f675f602d97789d5e2e2b35989e9fe3
DueB Software <admin@duebsoftware.it>
2026-09-24 14:49:52 +0000

    Removed dev with mike 2.2.0

--- dev/assets/js/extra.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-
-    // Set margin to table caption paragraph
-    var x = document.getElementsByClassName("table-caption");
-    var i;
-    for (i = 0; i < x.length; i++) {
-        x[i].parentNode.style.margin = "0em";
-    }
-}
